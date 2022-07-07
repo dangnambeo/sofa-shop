3acd75b2463339ed5e7855ee70234c5557a4899d
dangnambeo <dangnamnguyen99@gmail.com>
2022-07-07 18:45:31 +0700

    namnes

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\bill;
 use App\orders;
+use App\products;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class dashboardController extends Controller
 {
     public function dashboard(){
-        $sum_order= orders::sum('number');
-        return view('admin.dashboard.turnover',compact('sum_order'));
+        $bill= bill::all();
+        $bill_day=bill::whereDay('created_at', Carbon::now()->day)->get();
+        $bill_month=bill::whereMonth('created_at', Carbon::now()->month)->get();
+        $sp_bill = products::leftjoin('orders','orders.product_id','=','products.id')
+            ->leftjoin('bill','bill.id','=','orders.bill_id')
+            ->where('bill.status','!=',3)
+            ->groupBy('products.id','products.name','products.img','products.cate_id','products.quantity','products.price','products.description','products.discount_id')
+            ->select(
+                'products.id as id','products.name as name','products.img as img','products.cate_id as cate_id','products.quantity as quantity','products.price as price','products.description as description',
+                'products.discount_id as discount_id',
+                DB::raw('Sum(orders.number) as sl_ban')
+            )->orderBy('sl_ban','desc')
+            ->paginate(5);
+        return view('admin.dashboard.turnover',compact('bill_month', 'bill','bill_day','sp_bill'));
     }
 }
--- resources/views/admin/dashboard/turnover.blade.php
+++ resources/views/admin/dashboard/turnover.blade.php
@@ -20,9 +20,8 @@
                         <i class="mdi mdi-chart-areaspline font-30 widget-icon rounded-circle avatar-title text-primary"></i>
                     </div>
                     <div class="wigdet-one-content">
-                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="Statistics">Statistics</p>
-                        <h2><span data-plugin="counterup">{{ $sum_order }}</span> <i class="mdi mdi-arrow-up text-success font-24"></i></h2>
-                        <p class="text-muted m-0"><span class="font-weight-medium">Last:</span> 30.4k</p>
+                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="Statistics">Đơn hàng đã đặt</p>
+                        <h2><span data-plugin="counterup">{{ $bill->Count('id') }}</span></h2>
                     </div>
                 </div>
             </div>
@@ -36,9 +35,9 @@
                         <i class="mdi mdi-layers font-30 widget-icon rounded-circle avatar-title text-warning"></i>
                     </div>
                     <div class="wigdet-one-content">
-                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="User This Month">User This Month</p>
-                        <h2><span data-plugin="counterup">52410 </span> <i class="mdi mdi-arrow-up text-success font-24"></i></h2>
-                        <p class="text-muted m-0"><span class="font-weight-medium">Last:</span> 40.33k</p>
+                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="User This Month">Đơn hàng trong ngày</p>
+                        <h2><span data-plugin="counterup">{{ $bill_day->count('id') }} </span></h2>
+
                     </div>
                 </div>
             </div>
@@ -52,9 +51,9 @@
                         <i class="mdi mdi-av-timer font-30 widget-icon rounded-circle avatar-title text-danger"></i>
                     </div>
                     <div class="wigdet-one-content">
-                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="Statistics">Statistics</p>
-                        <h2><span data-plugin="counterup">6352 </span> <i class="mdi mdi-arrow-up text-success font-24"></i></h2>
-                        <p class="text-muted m-0"><span class="font-weight-medium">Last:</span> 956</p>
+                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="Statistics">Đơn hàng trong tháng</p>
+                        <h2><span data-plugin="counterup">{{ $bill_month->count('id') }}</span></h2>
+
                     </div>
                 </div>
             </div>
@@ -68,49 +67,15 @@
                         <i class="mdi mdi-account-convert font-30 widget-icon rounded-circle avatar-title text-success"></i>
                     </div>
                     <div class="wigdet-one-content">
-                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="User Today">User Today</p>
-                        <h2><span data-plugin="counterup">895</span> <i class="mdi mdi-arrow-down text-danger font-24"></i></h2>
-                        <p class="text-muted m-0"><span class="font-weight-medium">Last:</span> 1250</p>
+                        <p class="m-0 text-uppercase font-weight-bold text-muted" title="User Today">Đơn hàng giao thành công</p>
+                        <h2><span data-plugin="counterup">{{ $bill->where('status',2)->Count('id') }}</span></h2>
+
                     </div>
                 </div>
             </div>
         </div>
         <!-- end col -->
     </div>
-
-    <div class="row">
-        <div class="col-xl-6">
-            <div class="card-box">
-                <h4 class="header-title mb-4">Total Revenue</h4>
-
-                <div id="website-stats" style="height: 320px;" class="flot-chart"></div>
-            </div>
-        </div>
-
-        <div class="col-xl-6">
-            <div class="card-box">
-                <h4 class="header-title mb-4">Sales Analytics</h4>
-
-                <div class="float-right">
-                    <div id="reportrange" class="form-control form-control-sm">
-                        <i class="far fa-calendar-alt mr-1"></i>
-                        <span></span>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-
-                <div id="donut-chart">
-                    <div id="donut-chart-container" class="flot-chart" style="height: 246px;">
-                    </div>
-                </div>
-
-                <p class="text-muted mb-0 mt-3 text-truncate">Pie chart is used to see the proprotion of each data groups, making Flot pie chart is pretty simple, in order to make pie chart you have to incldue jquery.flot.pie.js plugin.</p>
-            </div>
-        </div>
-
-    </div>
-    <!-- end row -->
-
     <div class="row">
 
         <div class="col-lg-4">
@@ -191,85 +156,29 @@
 
         <div class="col-lg-8">
             <div class="card-box">
-                <h4 class="header-title mb-4">Recent Users</h4>
+                <h4 class="header-title mb-4">Sản phẩm bán chạy</h4>
 
                 <div class="table-responsive">
                     <table class="table table table-hover m-0">
                         <thead>
                         <tr>
-                            <th></th>
-                            <th>User Name</th>
-                            <th>Phone</th>
-                            <th>Location</th>
-                            <th>Date</th>
+                            <th>Mã sản phẩm</th>
+                            <th>Tên sản phẩm</th>
+                            <th>Sô lượng</th>
+                            <th>Giá bán</th>
                         </tr>
                         </thead>
                         <tbody>
+                        @foreach($sp_bill as $list)
                         <tr>
-                            <th>
-                                <img src="assets/images/users/avatar-6.jpg" alt="user" class="avatar-sm rounded-circle" />
-                            </th>
-                            <td>
-                                <h5 class="m-0 font-15">Louis Hansen</h5>
-                                <p class="m-0 text-muted font-13"><small>Web designer</small></p>
-                            </td>
-                            <td>+12 3456 789</td>
-                            <td>USA</td>
-                            <td>07/08/2016</td>
+                                <th>
+                                    {{ $list->id }}
+                                </th>
+                                <td>{{ $list ->name}}</td>
+                                <td>{{ ($list ->quantity)-($list->sl_ban) }}</td>
+                                <td>{{ number_format($list ->price) }} <span>₫</span> </td>
                         </tr>
-
-                        <tr>
-                            <th>
-                                <span class="avatar-sm-box bg-primary">C</span>
-                            </th>
-                            <td>
-                                <h5 class="m-0 font-15">Craig Hause</h5>
-                                <p class="m-0 text-muted font-13"><small>Programmer</small></p>
-                            </td>
-                            <td>+89 345 6789</td>
-                            <td>Canada</td>
-                            <td>29/07/2016</td>
-                        </tr>
-
-                        <tr>
-                            <th>
-                                <img src="assets/images/users/avatar-7.jpg" alt="user" class="avatar-sm rounded-circle" />
-                            </th>
-                            <td>
-                                <h5 class="m-0 font-15">Edward Grimes</h5>
-                                <p class="m-0 text-muted font-13"><small>Founder</small></p>
-                            </td>
-                            <td>+12 29856 256</td>
-                            <td>Brazil</td>
-                            <td>22/07/2016</td>
-                        </tr>
-
-                        <tr>
-                            <th>
-                                <span class="avatar-sm-box bg-pink">B</span>
-                            </th>
-                            <td>
-                                <h5 class="m-0 font-15">Bret Weaver</h5>
-                                <p class="m-0 text-muted font-13"><small>Web designer</small></p>
-                            </td>
-                            <td>+00 567 890</td>
-                            <td>USA</td>
-                            <td>20/07/2016</td>
-                        </tr>
-
-                        <tr>
-                            <th>
-                                <img src="assets/images/users/avatar-8.jpg" alt="user" class="avatar-sm rounded-circle" />
-                            </th>
-                            <td>
-                                <h5 class="m-0 font-15">Mark</h5>
-                                <p class="m-0 text-muted font-13"><small>Web design</small></p>
-                            </td>
-                            <td>+91 123 456</td>
-                            <td>India</td>
-                            <td>07/07/2016</td>
-                        </tr>
-
+                        @endforeach
                         </tbody>
                     </table>
 
